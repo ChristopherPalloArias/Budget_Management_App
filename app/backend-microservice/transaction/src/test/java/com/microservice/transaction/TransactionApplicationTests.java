9091ff9546f8f65c669f5ff718d985c2989a0552
Jacob <jacobowiz.7@protonmail.com>
2026-02-06 19:04:28 -0500

    fix jpa datasource

--- app/backend-microservice/transaction/src/test/java/com/microservice/transaction/TransactionApplicationTests.java
+++ app/backend-microservice/transaction/src/test/java/com/microservice/transaction/TransactionApplicationTests.java
@@ -4,14 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {
-                "spring.autoconfigure.exclude=" +
-                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"
-        })
+@SpringBootTest(classes = {})
 class TransactionApplicationTests {
 
 	@Test
